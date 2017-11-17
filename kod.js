90c4e2c7859639f45b11076fc7aefd2117dd638e
mayaaxelssonkaltak <mayaaxelssonkaltak@hotmail.com>
2017-11-17 13:19:27 +0100

    färgjquery

--- kod.js
+++ kod.js
@@ -1,5 +1,18 @@
+//kod.js
 $(function(){
     $("#openinfo").click(function(){
        $("#info").toggle();    
     });
-});
+    $("li").dblclick(function(){
+        $(this).toggleClass("selected");
+    });
+    $("#bgcolor").change(function(){
+        $("body").css("background-color", $(this).val());
+    });
+     $("#tecolor").change(function(){
+        $("body").css("color", $(this).val());
+    });
+     $("#rubrik").focusout(function(){
+        $("h1").text($(this).val());
+    });
+});  
